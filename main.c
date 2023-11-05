7fa4cccc69113df536fa415ac0b28cb667191630
Thiago K. S. Yamamoto <44378228+thiagoksy@users.noreply.github.com>
2023-11-04 21:26:50 -0300

    Update main.c
    Código inversor funcionando

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #define TempoAcionado 5000 // tempo acionado
 
 /*
- * CH- BA45FF00
- * CH  B946FF00
- * CH+ B847FF00
- * EQ F609FF00
- */
- 
+   CH- BA45FF00
+   CH  B946FF00
+   CH+ B847FF00
+   EQ F609FF00
+*/
+
 //pinos interrupções 2 e 3
 //INPUTS
 #define B0 2 //Botão de segurança B0
@@ -20,11 +20,10 @@
 #define SW_UP 6// Fim de curso Superior  SW1
 #define SW_DOWN 5//Fim de curso Inferior SW2
 
-#define RECEPTOR 4 
+#define RECEPTOR 4
 
 //OUTPUTS
-#define BUZZER 7 
-
+#define BUZZER 7
 
 #define RED_LIGHT 13
 #define GREEN_LIGHT 12
@@ -33,171 +32,184 @@
 #define B1_ACIONA A4
 #define B2_ACIONA A5
 
- typedef enum estados{
+typedef enum estados {
   OFF,
   ON,
   SUBIDA,
   EMERGENCIA,
   DESCIDA,
- }sts;
- 
- //Variáveis
-//IRrecv sensorIR(RECEPTOR);
+} sts;
+
+//Variáveis
+IRrecv sensorIR(RECEPTOR);
 
 unsigned long int tempo = 0;
 int inversor = 0;
 
 //Funções
-void etapa0(){ 
+void etapa0() {
   // Acende luz branca
   Serial.println("Etapa 0");
   digitalWrite(BUZZER, LOW);
-  digitalWrite(RED_LIGHT,LOW);
-  digitalWrite(WHITE_LIGHT,HIGH);
-  digitalWrite(GREEN_LIGHT,LOW); 
+  digitalWrite(RED_LIGHT, LOW);
+  digitalWrite(WHITE_LIGHT, HIGH);
+  digitalWrite(GREEN_LIGHT, LOW);
 }
 
-void etapa1_2(){ //etapa 1 e 2 nenhum fim de curso ativado
-  // Acende luz verde 
+void etapa1_2() { //etapa 1 e 2 nenhum fim de curso ativado
+  // Acende luz verde
   Serial.println("Etapa 1 e 2");
   digitalWrite(BUZZER, LOW); //colocar barulho
-  digitalWrite(RED_LIGHT,LOW);
-  digitalWrite(WHITE_LIGHT,LOW);
-  digitalWrite(GREEN_LIGHT,HIGH);  
-  
+  digitalWrite(RED_LIGHT, LOW);
+  digitalWrite(WHITE_LIGHT, LOW);
+  digitalWrite(GREEN_LIGHT, HIGH);
+
 }
 
-void etapa_3(){ //EMERGENCIA
-    // Acende luz vermelha e apaga as outras
-    // Fica apitando (alarme)
-    while(true){ 
-      Serial.println("Etapa 3");
-      digitalWrite(BUZZER, HIGH);
-      digitalWrite(RED_LIGHT,HIGH);
-      digitalWrite(WHITE_LIGHT,LOW);
-      digitalWrite(GREEN_LIGHT,LOW);
-      delay(500);
-      digitalWrite(BUZZER, LOW);
-      digitalWrite(RED_LIGHT,HIGH);
-      digitalWrite(WHITE_LIGHT,LOW);
-      digitalWrite(GREEN_LIGHT,LOW);
-      delay(500);
-    }
+void etapa3() { //EMERGENCIA
+  // Acende luz vermelha e apaga as outras
+  // Fica apitando (alarme)
+  while (true) {
+    Serial.println("Etapa 3");
+    digitalWrite(BUZZER, HIGH);
+    digitalWrite(RED_LIGHT, HIGH);
+    digitalWrite(WHITE_LIGHT, LOW);
+    digitalWrite(GREEN_LIGHT, LOW);
+    delay(500);
+    digitalWrite(BUZZER, LOW);
+    digitalWrite(RED_LIGHT, HIGH);
+    digitalWrite(WHITE_LIGHT, LOW);
+    digitalWrite(GREEN_LIGHT, LOW);
+    delay(500);
+  }
 }
 
-void etapa4(){// Fins de curso  atingido
+void etapa4() { // Fins de curso  atingido
   // Pisca Luz branca e dá apito
+  tempo = millis();
+  int estado = 0;
   Serial.println("Etapa 4");
+  while (millis() - tempo < TempoAcionado) {
+    if(millis()%200 == 0){
+      estado = !estado;
+    }
+    digitalWrite(WHITE_LIGHT, estado);
+    digitalWrite(BUZZER, estado);
+  }
   digitalWrite(BUZZER, LOW); //colcoar barulho
-  digitalWrite(RED_LIGHT,LOW);
-  digitalWrite(GREEN_LIGHT,LOW);
-
-  digitalWrite(WHITE_LIGHT,HIGH);
-  delay(500);
-  digitalWrite(BUZZER, LOW); //colcoar barulho
-  digitalWrite(WHITE_LIGHT,LOW);
-  delay(500);
+  digitalWrite(WHITE_LIGHT, LOW);
 }
 
-void barreira(){
-  while(millis()-tempo >= TempoAcionado){
+void etapa5() { //Barreira
+  tempo = millis();
+  int estado = 0;
+  Serial.println("Etapa 5");
+  while (millis() - tempo < TempoAcionado) {
     digitalWrite(B1_ACIONA, HIGH);
+    if(millis()%100 == 0){
+      estado = !estado;
+    }
+    digitalWrite(WHITE_LIGHT, estado);
+    digitalWrite(BUZZER, estado);
   }
   digitalWrite(B1_ACIONA, LOW);
-}
-
-void setup(){
-  Serial.begin(9600);
-//  sensorIR.enableIRIn();
- 
-  
-  pinMode(B0,INPUT);
-  pinMode(B1,INPUT);
-  pinMode(B2,INPUT);
-  pinMode(LDR,INPUT);
-  pinMode(SW_UP,INPUT);
-  pinMode(SW_DOWN,INPUT);
- 
-  pinMode(BUZZER,OUTPUT);
-  pinMode(RED_LIGHT,OUTPUT);
-  pinMode(WHITE_LIGHT,OUTPUT);
-  pinMode(GREEN_LIGHT,OUTPUT);
-  pinMode(B1_ACIONA,OUTPUT);
-  pinMode(B2_ACIONA,OUTPUT);
-  
   digitalWrite(BUZZER, LOW);
-  digitalWrite(RED_LIGHT,LOW);
-  digitalWrite(WHITE_LIGHT,LOW);
-  digitalWrite(GREEN_LIGHT,LOW);
-  
-  digitalWrite(B1_ACIONA,LOW);
-  digitalWrite(B2_ACIONA,LOW);
-  etapa0;
-  attachInterrupt(B0, etapa_3, FALLING);
-  attachInterrupt(LDR, barreira, RISING);
-  inversor =0;
-   
+  Serial.println("Fim da Etapa 5");
+  digitalWrite(WHITE_LIGHT, LOW);
 }
 
-void loop(){
- if(digitalRead(B0)==LOW){
+void setup() {
+  Serial.begin(9600);
+  sensorIR.enableIRIn();
+
+
+  pinMode(B0, INPUT);
+  pinMode(B1, INPUT);
+  pinMode(B2, INPUT);
+  pinMode(LDR, INPUT);
+  pinMode(SW_UP, INPUT);
+  pinMode(SW_DOWN, INPUT);
+
+  pinMode(BUZZER, OUTPUT);
+  pinMode(RED_LIGHT, OUTPUT);
+  pinMode(WHITE_LIGHT, OUTPUT);
+  pinMode(GREEN_LIGHT, OUTPUT);
+  pinMode(B1_ACIONA, OUTPUT);
+  pinMode(B2_ACIONA, OUTPUT);
+
+  digitalWrite(BUZZER, LOW);
+  digitalWrite(RED_LIGHT, LOW);
+  digitalWrite(WHITE_LIGHT, LOW);
+  digitalWrite(GREEN_LIGHT, LOW);
+
+  digitalWrite(B1_ACIONA, LOW);
+  digitalWrite(B2_ACIONA, LOW);
+  etapa0;
+  attachInterrupt(B0, etapa3, FALLING);
+  attachInterrupt(LDR, etapa5, RISING);
+  inversor = 0;
+
+}
+
+void loop() {
+  if (digitalRead(B0) == LOW) {
     inversor = 1;
-    etapa_3();
+    etapa3();
   }
-  else{
-       if(digitalRead(B1) == LOW || digitalRead(B2)==LOW){
-         inversor = 1;
-         etapa1_2();
-       }
-    else{
-        etapa0();
-       }
+  else {
+    if (digitalRead(B1) == LOW || digitalRead(B2) == LOW) {
+      inversor = 1;
+      etapa1_2();
+    }
+    else {
+      etapa0();
+    }
   }
-  if(digitalRead(SW_UP) == LOW){
-    etapa4(); 
+  if (digitalRead(SW_UP) == LOW) {
+    etapa4();
   }
-  if(digitalRead(SW_UP) == LOW){
-    
+  if (digitalRead(SW_DOWN) == LOW) {
+    etapa5();
   }
- /*
-  if(sensorIR.decode() && inversor == 0){
-      switch(sensorIR.decodedIRData.decodedRawData){
-        case 0xF609BF00:{ // descida
-          Serial.println("CH-");
-          digitalWrite(B1_ACIONA,LOW);
-          digitalWrite(B2_ACIONA,HIGH);
-          etapa1_2();
-          digitalWrite(B1_ACIONA,LOW);
-          digitalWrite(B2_ACIONA,LOW);
-          break;
+  /*
+    if(sensorIR.decode() && inversor == 0){
+       switch(sensorIR.decodedIRData.decodedRawData){
+         case 0xF609BF00:{ // descida
+           Serial.println("CH-");
+           digitalWrite(B1_ACIONA,LOW);
+           digitalWrite(B2_ACIONA,HIGH);
+           etapa1_2();
+           digitalWrite(B1_ACIONA,LOW);
+           digitalWrite(B2_ACIONA,LOW);
+           break;
+         }
+         case 0xFF00BF00:{ // emergencia
+           digitalWrite(B1_ACIONA,LOW);
+           digitalWrite(B2_ACIONA,LOW);
+           etapa_3();
+           break;
+         }
+         case 0xFE01BF00:{ // subida
+           digitalWrite(B1_ACIONA,HIGH);
+           digitalWrite(B2_ACIONA,LOW);
+           Serial.println("CH+");
+           etapa1_2();
+           digitalWrite(B1_ACIONA,LOW);
+           digitalWrite(B2_ACIONA,LOW);
+           break;
+         }
+         case 0xF609FF00:{
+           Serial.println("EQ");
+           break;
+         }
+         default:{
+            etapa0();
+           Serial.println("NULO");
+           break;
+         }
+
         }
-        case 0xFF00BF00:{ // emergencia
-          digitalWrite(B1_ACIONA,LOW);
-          digitalWrite(B2_ACIONA,LOW);
-          etapa_3();
-          break;
-        }
-        case 0xFE01BF00:{ // subida
-          digitalWrite(B1_ACIONA,HIGH);
-          digitalWrite(B2_ACIONA,LOW);
-          Serial.println("CH+");
-          etapa1_2();
-          digitalWrite(B1_ACIONA,LOW);
-          digitalWrite(B2_ACIONA,LOW);
-          break;
-        }
-        case 0xF609FF00:{
-          Serial.println("EQ");
-          break;
-        }
-        default:{
-           etapa0();
-          Serial.println("NULO");
-          break;
-        }
-          
-       }
-    delay(5000);
-    sensorIR.resume();
-   }*/
+     delay(5000);
+     sensorIR.resume();
+    }*/
 }
